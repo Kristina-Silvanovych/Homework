ab15d2d30bbd880efa1862a8eafe06b6ee478522
Kristina-Silvanovych <64775345+Kristina-Silvanovych@users.noreply.github.com>
2020-11-09 14:56:28 +0200

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,19 +1,12 @@
 "use strict";
 //let str1 = "Hello spam!";
 //let str2 = "Hello world!";
-let spam = "spam";
-
 function check(str) {
   str = str.toLowerCase();
-  let p = str.indexOf("spam");
-  if (str.indexOf("spam") != -1) {
-    let n = str.replace("spam", "***");
-    p = str.indexOf("spam", p + 4);
+    let n = str.replace(/spam/g, "***");
     return n;
-  } else {
-    return false;
-  }
   //return str.search(spam.toLowerCase()) >= 0 ? true : false;
 }
 let str1 = prompt("Enter a sentence");
 alert(check(str1));
+
